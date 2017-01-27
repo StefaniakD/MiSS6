bb51460273e9b760c9edc60faddf68b2727dacba
Dominik Stefaniak <stefaniak.dominik@gmail.com>
2017-01-27 07:29:26 +0100

    Almost fcking done...

--- MiSS6/Form1.Designer.cs
+++ MiSS6/Form1.Designer.cs
@@ -28,29 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBoxEquation = new System.Windows.Forms.PictureBox();
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBoxTStart = new System.Windows.Forms.TextBox();
-            this.textBoxTEnd = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBoxTempo = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBoxR = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.buttonCalculate = new System.Windows.Forms.Button();
             this.buttonStartPause = new System.Windows.Forms.Button();
             this.buttonReverse = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDownTStart = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownTEnd = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownTempo = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownR = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEquation)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTEnd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTempo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownR)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxEquation
             // 
             this.pictureBoxEquation.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxEquation.Image")));
-            this.pictureBoxEquation.Location = new System.Drawing.Point(246, 12);
+            this.pictureBoxEquation.Location = new System.Drawing.Point(404, 12);
             this.pictureBoxEquation.Name = "pictureBoxEquation";
             this.pictureBoxEquation.Size = new System.Drawing.Size(239, 85);
             this.pictureBoxEquation.TabIndex = 0;
@@ -58,72 +64,47 @@
             // 
             // pictureBox
             // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox.Location = new System.Drawing.Point(12, 218);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(697, 269);
+            this.pictureBox.Size = new System.Drawing.Size(1000, 400);
             this.pictureBox.TabIndex = 1;
             this.pictureBox.TabStop = false;
+            this.pictureBox.WaitOnLoad = true;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(72, 120);
+            this.label1.Location = new System.Drawing.Point(230, 120);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "t_start [s]";
             // 
-            // textBoxTStart
-            // 
-            this.textBoxTStart.Location = new System.Drawing.Point(128, 117);
-            this.textBoxTStart.Name = "textBoxTStart";
-            this.textBoxTStart.Size = new System.Drawing.Size(43, 20);
-            this.textBoxTStart.TabIndex = 3;
-            // 
-            // textBoxTEnd
-            // 
-            this.textBoxTEnd.Location = new System.Drawing.Point(128, 143);
-            this.textBoxTEnd.Name = "textBoxTEnd";
-            this.textBoxTEnd.Size = new System.Drawing.Size(43, 20);
-            this.textBoxTEnd.TabIndex = 5;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(74, 146);
+            this.label2.Location = new System.Drawing.Point(232, 146);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "t_end [s]";
             // 
-            // textBoxTempo
-            // 
-            this.textBoxTempo.Location = new System.Drawing.Point(128, 169);
-            this.textBoxTempo.Name = "textBoxTempo";
-            this.textBoxTempo.Size = new System.Drawing.Size(43, 20);
-            this.textBoxTempo.TabIndex = 7;
-            this.textBoxTempo.Text = "1";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(72, 172);
+            this.label3.Location = new System.Drawing.Point(230, 172);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "tempo [*]";
             // 
-            // textBoxR
-            // 
-            this.textBoxR.Location = new System.Drawing.Point(246, 117);
-            this.textBoxR.Name = "textBoxR";
-            this.textBoxR.Size = new System.Drawing.Size(43, 20);
-            this.textBoxR.TabIndex = 9;
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(205, 120);
+            this.label4.Location = new System.Drawing.Point(363, 120);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(32, 13);
             this.label4.TabIndex = 8;
@@ -131,57 +112,158 @@
             // 
             // buttonCalculate
             // 
-            this.buttonCalculate.Location = new System.Drawing.Point(328, 167);
+            this.buttonCalculate.Location = new System.Drawing.Point(486, 167);
             this.buttonCalculate.Name = "buttonCalculate";
             this.buttonCalculate.Size = new System.Drawing.Size(75, 23);
             this.buttonCalculate.TabIndex = 10;
             this.buttonCalculate.Text = "Oblicz";
             this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
             // 
             // buttonStartPause
             // 
-            this.buttonStartPause.Location = new System.Drawing.Point(328, 505);
+            this.buttonStartPause.Enabled = false;
+            this.buttonStartPause.Location = new System.Drawing.Point(509, 624);
             this.buttonStartPause.Name = "buttonStartPause";
             this.buttonStartPause.Size = new System.Drawing.Size(75, 23);
             this.buttonStartPause.TabIndex = 11;
             this.buttonStartPause.Text = "Start";
             this.buttonStartPause.UseVisualStyleBackColor = true;
+            this.buttonStartPause.Click += new System.EventHandler(this.buttonStartPause_Click);
             // 
             // buttonReverse
             // 
-            this.buttonReverse.Location = new System.Drawing.Point(246, 505);
+            this.buttonReverse.Enabled = false;
+            this.buttonReverse.Location = new System.Drawing.Point(428, 625);
             this.buttonReverse.Name = "buttonReverse";
             this.buttonReverse.Size = new System.Drawing.Size(75, 23);
             this.buttonReverse.TabIndex = 12;
             this.buttonReverse.Text = "<<";
             this.buttonReverse.UseVisualStyleBackColor = true;
+            this.buttonReverse.Click += new System.EventHandler(this.buttonReverse_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(512, 539);
+            this.label5.Location = new System.Drawing.Point(841, 639);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(197, 9);
             this.label5.TabIndex = 13;
             this.label5.Text = "Anna Cholewczyńska, Kamil Gersten, Dominik Stefaniak";
             // 
+            // numericUpDownTStart
+            // 
+            this.numericUpDownTStart.DecimalPlaces = 1;
+            this.numericUpDownTStart.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownTStart.Location = new System.Drawing.Point(286, 118);
+            this.numericUpDownTStart.Name = "numericUpDownTStart";
+            this.numericUpDownTStart.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDownTStart.TabIndex = 14;
+            // 
+            // numericUpDownTEnd
+            // 
+            this.numericUpDownTEnd.DecimalPlaces = 1;
+            this.numericUpDownTEnd.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownTEnd.Location = new System.Drawing.Point(286, 144);
+            this.numericUpDownTEnd.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDownTEnd.Name = "numericUpDownTEnd";
+            this.numericUpDownTEnd.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDownTEnd.TabIndex = 15;
+            this.numericUpDownTEnd.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownTempo
+            // 
+            this.numericUpDownTempo.DecimalPlaces = 1;
+            this.numericUpDownTempo.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownTempo.Location = new System.Drawing.Point(285, 170);
+            this.numericUpDownTempo.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDownTempo.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownTempo.Name = "numericUpDownTempo";
+            this.numericUpDownTempo.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDownTempo.TabIndex = 16;
+            this.numericUpDownTempo.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownR
+            // 
+            this.numericUpDownR.DecimalPlaces = 2;
+            this.numericUpDownR.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownR.Location = new System.Drawing.Point(401, 118);
+            this.numericUpDownR.Maximum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDownR.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownR.Name = "numericUpDownR";
+            this.numericUpDownR.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDownR.TabIndex = 17;
+            this.numericUpDownR.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(721, 557);
+            this.ClientSize = new System.Drawing.Size(1027, 657);
+            this.Controls.Add(this.numericUpDownR);
+            this.Controls.Add(this.numericUpDownTempo);
+            this.Controls.Add(this.numericUpDownTEnd);
+            this.Controls.Add(this.numericUpDownTStart);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.buttonReverse);
             this.Controls.Add(this.buttonStartPause);
             this.Controls.Add(this.buttonCalculate);
-            this.Controls.Add(this.textBoxR);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBoxTempo);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBoxTEnd);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBoxTStart);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.pictureBoxEquation);
@@ -189,6 +271,10 @@
             this.Text = "MiSS6 - Symulator Falowania Wody";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEquation)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTEnd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTempo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownR)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -199,17 +285,18 @@
         private System.Windows.Forms.PictureBox pictureBoxEquation;
         private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBoxTStart;
-        private System.Windows.Forms.TextBox textBoxTEnd;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBoxTempo;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBoxR;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button buttonCalculate;
         private System.Windows.Forms.Button buttonStartPause;
         private System.Windows.Forms.Button buttonReverse;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDownTStart;
+        private System.Windows.Forms.NumericUpDown numericUpDownTEnd;
+        private System.Windows.Forms.NumericUpDown numericUpDownTempo;
+        private System.Windows.Forms.NumericUpDown numericUpDownR;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- MiSS6/Form1.cs
+++ MiSS6/Form1.cs
@@ -1,20 +1,190 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        public const int XSIZE = 1000;
+        public const int ZSIZE = 400;
+
+        public int FramesNumber;
+        public double[,,,] Data;
+
+        Bitmap bm;
+        Graphics bmg;
+
+        int ActualFrame = 0;
+
+        public bool IsPlaying = false;
+        public bool IsReversed = false;
+
         public Form1()
         {
             InitializeComponent();
+            this.pictureBox.Paint += new PaintEventHandler(pictureBox_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.Controls.Add(this.pictureBox);
+
+            this.timer1.Tick += new EventHandler(timer1_Tick);
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            if (numericUpDownTEnd.Value <= numericUpDownTStart.Value) MessageBox.Show("t_end musi być większe niż t_start");
+            else
+            {
+                this.buttonCalculate.Enabled = false;
+                this.numericUpDownTStart.Enabled = false;
+                this.numericUpDownTEnd.Enabled = false;
+                this.numericUpDownTempo.Enabled = false;
+                this.numericUpDownR.Enabled = false;
+                this.buttonStartPause.Enabled = false;
+                this.buttonReverse.Enabled = false;
+
+                FramesNumber = (int)(numericUpDownTEnd.Value - numericUpDownTStart.Value)*30;
+                Data = new double[FramesNumber, XSIZE, 2, 1];
+
+                double R = (double)numericUpDownR.Value*100;
+                double L = 2 * Math.PI * R;
+                double c = 1.25 * Math.Sqrt(L);
+                double w = (2*Math.PI)/ Math.Sqrt(L);
+
+                System.IO.StreamWriter file = new System.IO.StreamWriter(@"data.txt");
+
+                for(int frame = 0; frame < FramesNumber; frame++)
+                {
+                    file.Write(frame + ": ");
+                    for(int x=0; x < XSIZE; x++)
+                    {
+                        double temp = (x - (c * frame /30)) /R;
+                        if (x == 0) file.Write(temp + " ");
+                        if (temp < -1)
+                        {
+                            if ((int)temp % 2 == 0) temp = temp - (int)temp;
+                            if ((int)temp % 2 == -1) temp = -1 - (temp - (int)temp);
+                        }
+
+                        if (temp > 1)
+                        {
+                            if ((int)temp % 2 == 0) temp = temp - (int)temp;
+                            if ((int)temp % 2 == 1) temp = 1 - (temp - (int)temp);
+                        }
+                        if (x == 0) file.Write(temp + " ");
+                        double arccos = Math.Acos(temp);
+
+                        double omega;
+                        if (frame%30 ==0) omega = 0;
+                        else omega = arccos / frame /30;
+                        double z = R * Math.Sin(omega * frame/30);
+
+                        Data[frame, x, 0, 0] = x;
+                        Data[frame, x, 1, 0] = z;
+
+                        
+                    }file.WriteLine(Data[frame, 1, 1, 0] + " ");
+                    //MessageBox.Show(" ");
+                }
+
+                this.buttonCalculate.Enabled = true;
+                this.numericUpDownTStart.Enabled = true;
+                this.numericUpDownTEnd.Enabled = true;
+                this.numericUpDownTempo.Enabled = true;
+                this.numericUpDownR.Enabled = true;
+                this.buttonStartPause.Enabled = true;
+
+            }
+        }
+        
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (bm == null)
+            {
+                bm = new Bitmap(XSIZE, ZSIZE);
+                bmg = Graphics.FromImage(bm);
+                bmg.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                bmg.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.GammaCorrected;
+                bmg.Clear(Color.White);
+            }
+            if (IsPlaying == true) Render();
+            e.Graphics.DrawImage(bm, 0, 0);
+        }
+
+        void Render()
+        {
+            bmg.Clear(Color.White);
+            int wsp = 500;
+            double x0 = 0;
+            double z0 = 0;
+            for(int i = 0; i < XSIZE; i++)
+            {
+                double x = Data[ActualFrame, i, 0, 0];
+                double z = Data[ActualFrame, i, 1, 0];
+                if (z > 0) z = 200 + z * wsp;
+                if (z < 0) z = 200 - z * wsp;
+                if (i != 0) bmg.DrawLine(Pens.Blue, (float)x0, (float)z0, (float)x, (float)z);
+                x0 = x;
+                z0 = z;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (ActualFrame == FramesNumber - 1)
+            {
+                this.timer1.Stop();
+                ActualFrame = 0;
+                IsPlaying = false;
+                this.buttonStartPause.Text = "Start";
+                bmg.Clear(Color.White);
+                this.pictureBox.Invalidate();
+            }
+            else if (IsReversed == true && ActualFrame == 0)
+            {
+                ActualFrame = 0;
+                IsPlaying = false;
+                IsReversed = false;
+                this.buttonStartPause.Text = "Start";
+                this.timer1.Stop();
+                bmg.Clear(Color.White);
+                this.pictureBox.Invalidate();
+            }
+            else
+            {
+                if (IsReversed == false && ActualFrame < FramesNumber - 1) ActualFrame++;
+                else if (ActualFrame > 0) ActualFrame--;
+                this.pictureBox.Invalidate();
+            }
+        }
+
+        private void buttonStartPause_Click(object sender, EventArgs e)
+        {
+            if (IsPlaying == false)
+            {
+                this.timer1.Interval = (int)(30 / this.numericUpDownTempo.Value);
+                IsPlaying = true;
+                this.buttonStartPause.Text = "Pause";
+                this.timer1.Start();
+            }
+            else
+            {
+                this.timer1.Stop();
+                IsPlaying = false;
+                this.buttonStartPause.Text = "Start";
+            }
+            this.buttonReverse.Enabled = true;
+        }
+
+        private void buttonReverse_Click(object sender, EventArgs e)
+        {
+            if (IsPlaying == false)
+            {
+                IsPlaying = true;
+                IsReversed = true;
+                this.timer1.Start();
+                this.buttonStartPause.Text = "Pause";
+            }
         }
     }
 }
